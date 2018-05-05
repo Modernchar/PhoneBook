38c4f753f10606f0fc716913a5d2570411885053
scutpaul <jszxchx@126.com>
2018-05-05 10:29:41 +0800

    将二维码项目添加进项目，一个是库，一个是activity

---
+++ app/src/main/java/cn/edu/scut/phonebook/Bit_scan.java
@@ -0,0 +1,13 @@
+package cn.edu.scut.phonebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bit_scan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bit_scan);
+    }
+}
