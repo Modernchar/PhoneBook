5bc649495ab45349baed6b7751c02238e4a636f8
scutpaul <jszxchx@126.com>
2018-05-05 10:31:48 +0800

    将原phonebook的sdk升级到27，并加入buildtoolversion

--- app/src/main/java/cn/edu/scut/phonebook/Bit_scan.java
+++ app/src/main/java/cn/edu/scut/phonebook/Bit_scan.java
@@ -3,11 +3,20 @@ package cn.edu.scut.phonebook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.zxing.WriterException;
+import com.yanzhenjie.permission.Action;
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.Permission;
+import com.yzq.zxinglibrary.android.CaptureActivity;
+import com.yzq.zxinglibrary.bean.ZxingConfig;
+import com.yzq.zxinglibrary.common.Constant;
+import com.yzq.zxinglibrary.encode.CodeCreator;
+
 public class Bit_scan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bit_scan);
+        //setContentView(R.layout.activity_bit_scan);
     }
 }
